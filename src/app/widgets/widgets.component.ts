82a78a55cd840c7b51d4208229889c348d3d99ec
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2018-12-27 17:18:30 -0300

    add mock widget objects; display widgets collection using ngFor directive; use of an event binding to set selected widget to component property; display of widget properties using property binding and interpolation binding; use of ngIf directive to show an alternate message if no widget is selected

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -1,4 +1,4 @@
-import { OnInit, Component } from '@angular/core';
+import { OnInit, Component, Input } from '@angular/core';
 
 // decorate widget component to use the template
 @Component({
@@ -8,10 +8,52 @@ import { OnInit, Component } from '@angular/core';
 })
 export class WidgetComponent implements OnInit {
 
-  message = 'Hello Widget';
+  widgets = [
+    {
+      id: 1,
+      name: 'Red Widget',
+      description: 'This is a red widget'
+    },
+    {
+      id: 2,
+      name: 'Orange Widget',
+      description: 'This is an orange widget'
+    },
+    {
+      id: 3,
+      name: 'Yellow Widget',
+      description: 'This is a yellow widget'
+    },
+    {
+      id: 4,
+      name: 'Green Widget',
+      description: 'This is a green widget'
+    },
+    {
+      id: 5,
+      name: 'Blue Widget',
+      description: 'This is a blue widget'
+    },
+    {
+      id: 6,
+      name: 'Indigo Widget',
+      description: 'This is a indigo widget'
+    },
+    {
+      id: 7,
+      name: 'Violet Widget',
+      description: 'This is a violet widget'
+    }
+  ];
+
+  selectedWidget;
 
   ngOnInit(): void {
     console.log('implement this if u need')
   }
 
+  // assign the selected widget to the selectedWidget property
+  selected(widget) {
+    this.selectedWidget = widget;
+  }
 }
