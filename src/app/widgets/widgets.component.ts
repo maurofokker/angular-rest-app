673a0b949ac690d8e1e4af27c77317fb8804e0f3
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2018-12-28 14:36:15 -0300

    create widgets service with the widget collection, consume and display from component and widget interface

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -1,4 +1,5 @@
 import { OnInit, Component, Input } from '@angular/core';
+import { WidgetsService, Widget } from 'app/shared';
 
 // decorate widget component to use the template
 @Component({
@@ -8,48 +9,14 @@ import { OnInit, Component, Input } from '@angular/core';
 })
 export class WidgetComponent implements OnInit {
 
-  widgets = [
-    {
-      id: 1,
-      name: 'Red Widget',
-      description: 'This is a red widget'
-    },
-    {
-      id: 2,
-      name: 'Orange Widget',
-      description: 'This is an orange widget'
-    },
-    {
-      id: 3,
-      name: 'Yellow Widget',
-      description: 'This is a yellow widget'
-    },
-    {
-      id: 4,
-      name: 'Green Widget',
-      description: 'This is a green widget'
-    },
-    {
-      id: 5,
-      name: 'Blue Widget',
-      description: 'This is a blue widget'
-    },
-    {
-      id: 6,
-      name: 'Indigo Widget',
-      description: 'This is a indigo widget'
-    },
-    {
-      id: 7,
-      name: 'Violet Widget',
-      description: 'This is a violet widget'
-    }
-  ];
+  selectedWidget: Widget;
+  widgets: Widget[];
 
-  selectedWidget;
+  constructor(private widgetsService: WidgetsService) {}
 
   ngOnInit(): void {
-    console.log('implement this if u need')
+    console.log('load widgets');
+    this.widgets = this.widgetsService.loadWidgets();
   }
 
   // assign the selected widget to the selectedWidget property
